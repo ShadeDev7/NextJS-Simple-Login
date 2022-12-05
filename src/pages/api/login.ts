8a76f58206fe309a1a3c278782dad0f3f5911983
ShadeDev7 <shadedev7@gmail.com>
2022-12-05 02:38:19 -0300

    Fix compilation error

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             return res.status(400).json({ error: "Invalid username or password!" });
         }
 
-        const matches = await comparePassword(password, account.password);
+        const matches = await comparePassword(password, account.password!);
         if (!matches) {
             return res.status(400).json({ error: "Invalid username or password!" });
         }
